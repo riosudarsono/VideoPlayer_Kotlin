aad80cc894335e294fba7af248c6482b84ebcd38
Rio Sudarsono <rio.sudarsono@crealoka.com>
2023-05-01 09:46:32 +0700

    feature: view video fullscreen

---
+++ app/src/main/java/com/rio/videoplayer/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.rio.videoplayer.utils
+
+object Constants {
+    const val DATA_EXTRA = "data_extra"
+}
--- app/src/main/java/com/rio/videoplayer/view/video/VideoFullActivity.kt
+++ app/src/main/java/com/rio/videoplayer/view/video/VideoFullActivity.kt
@@ -1,12 +1,12 @@
-package com.rio.videoplayer.view
+package com.rio.videoplayer.view.video
 
-import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -18,71 +18,57 @@ import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import com.rio.videoplayer.R
-import com.rio.videoplayer.data.response.ResultResponse
-import com.rio.videoplayer.utils.*
-import com.rio.videoplayer.view.video.VideoFullActivity
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
+import com.rio.videoplayer.utils.BaseApp
+import com.rio.videoplayer.utils.Constants
+import com.rio.videoplayer.utils.toast
+import kotlinx.android.synthetic.main.activity_video_full.*
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity(), BaseApp.Listener, VideoAdapter.Listener {
-
-    private val viewModel: MainVM by viewModels()
-    private lateinit var adapter: VideoAdapter
+class VideoFullActivity : AppCompatActivity(), BaseApp.Listener {
 
     private lateinit var simpleExoPlayer: ExoPlayer
     private lateinit var mediaDataSourceFactory: DataSource.Factory
-    private  var linkVideo: String = ""
+
+    private var linkVideo: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_video_full)
         BaseApp(this).set()
     }
 
     override fun getIntentData() {
-
-    }
-
-    override fun setOnClick() {
-        iv_full.setOnClickListener {
-            val i = Intent(this, VideoFullActivity::class.java)
-            i.putExtra(Constants.DATA_EXTRA, linkVideo)
-            startActivity(i)
+        if (intent.hasExtra(Constants.DATA_EXTRA)){
+            linkVideo = intent.getStringExtra(Constants.DATA_EXTRA)
+        } else {
+            toast("Data Kosong")
+            finish()
         }
     }
 
+    override fun setOnClick() {
+        iv_close.setOnClickListener { finish() }
+    }
+
     override fun setAdapter() {
-        adapter = VideoAdapter(this, ArrayList())
-        rv_video.adapter = adapter
-        rv_video.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
     }
 
     override fun setContent() {
-
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val params = playerView.layoutParams as RelativeLayout.LayoutParams
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        playerView.layoutParams = params
     }
 
     override fun loadData() {
-        viewModel.search("jack+johnson").observe(this, Observer {
-            when (it.status) {
-                Status.LOADING -> progressBar.show()
-                Status.SUCCESS -> {
-                    progressBar.hide()
-                    adapter.setItems(it.data ?: ArrayList())
-                }
-                Status.ERROR -> {
-                    progressBar.hide()
-                    toast(it.message)
-                }
-            }
-        })
-    }
 
-    override fun onClick(data: ResultResponse) {
-        if (linkVideo.isNotEmpty()) releasePlayer()
-        linkVideo = data.previewUrl ?: ""
-        initializePlayer()
-        rl_video.show()
     }
 
     private fun initializePlayer() {
@@ -99,7 +85,7 @@ class MainActivity : AppCompatActivity(), BaseApp.Listener, VideoAdapter.Listene
 
         simpleExoPlayer.addMediaSource(mediaSource)
         simpleExoPlayer.playWhenReady = true
-        simpleExoPlayer.repeatMode = Player.REPEAT_MODE_OFF;
+        simpleExoPlayer.repeatMode = Player.REPEAT_MODE_ALL;
         simpleExoPlayer.prepare()
 
         playerView.setShutterBackgroundColor(Color.TRANSPARENT)
